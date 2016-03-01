09338f0359f63ce5ec647c8368a8cddd20921425
Scott <sfrancis@misinteractive.com>
2016-03-01 02:32:44 -0500

    Added css file to distro

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 
 var gulp=require('gulp');
 var sassdoc = require('sassdoc');
+var sass = require('gulp-sass');
+var concat=require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
 
 gulp.task('default',function(){
     console.log('echo material design font...');
@@ -11,3 +15,17 @@ gulp.task('sassdoc', function () {
         .pipe(sassdoc());
 
 });
+
+
+gulp.task('dist', function () {
+    compileSass('md.fonts.scss');
+});
+
+
+
+function compileSass(src){
+    gulp.src('./src/' + src)
+        .pipe(sass())
+        .pipe(gulp.dest('./dist'));
+}
+
